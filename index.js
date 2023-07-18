e5e2166f33f5a5d26c71ea55ba6705cdafda9dfd
SeberyDev <sebasmanager06@gmail.com>
2023-07-18 17:05:39 -0600

    Add authentication

---
+++ index.js
@@ -0,0 +1,58 @@
+import app from "./app.js";
+import { createConnection } from "./lib/database/index.js";
+import debug from "debug";
+import http from "http";
+
+createConnection();
+
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+
+  if (port >= 0) {
+    return port;
+  }
+
+  return false;
+};
+
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+const server = http.createServer(app);
+
+server.listen(port);
+
+const onError = (error) => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+server.on("error", onError);
+
+const onListening = () => {
+  const addr = server.address();
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  debug("challenge-zerocopylabs:server")("Listening on " + bind);
+};
+
+server.on("listening", onListening);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,83 @@
-var express = require('express');
-var router = express.Router();
+import express from "express";
+import passport from "passport";
+import LocalStrategy from "passport-local";
+import { getDB } from "../lib/database/index.js";
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+passport.use(
+  new LocalStrategy(function verify(username, password, cb) {
+    const formattedUsername = username.trim().toLowerCase();
+    const formattedPassword = password.trim().toLowerCase();
+
+    const userFound = getDB().data.users.find(
+      (user) =>
+        formattedUsername === user.email &&
+        formattedPassword === user.password &&
+        user.isActive
+    );
+
+    if (!userFound) return cb(null, false);
+
+    return cb(null, userFound);
+  })
+);
+
+passport.serializeUser(function (user, cb) {
+  process.nextTick(function () {
+    cb(null, { id: user.guid, username: user.email });
+  });
 });
 
-module.exports = router;
+passport.deserializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, user);
+  });
+});
+
+const router = express.Router();
+
+router.get(
+  "/",
+  (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next();
+    } else {
+      res.redirect("/login");
+    }
+  },
+  (req, res, next) => {
+    res.render("index");
+  }
+);
+
+router.get(
+  "/login",
+  (req, res, next) => {
+    if (req.isAuthenticated()) {
+      res.redirect("/");
+    } else {
+      return next();
+    }
+  },
+  (req, res, next) => {
+    res.render("login");
+  }
+);
+
+router.post(
+  "/login/password",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+  })
+);
+
+router.post("/logout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/login");
+  });
+});
+
+export default router;
